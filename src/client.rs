e4b0701aa08d3e63ca099334e06e021f80f4b317
Anthony Nowell <anowell@gmail.com>
2021-03-17 00:54:43 -0700

    Documentation tweaks

--- src/client.rs
+++ src/client.rs
@@ -9,6 +9,7 @@ use crate::error::Result;
 
 static USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
+/// API client for chess.com
 pub struct ChessApi {
     config: Configuration,
 }
@@ -21,20 +22,24 @@ impl ChessApi {
         ChessApi { config }
     }
 
+    /// Get profile details about a user
     pub async fn get_profile(&self, username: &str) -> Result<api::Profile> {
         pub_player_username_get(&self.config, username).await
     }
 
+    /// Check if the user has been online in the last 5 minutes
     pub async fn is_user_online(&self, username: &str) -> Result<bool> {
         pub_player_username_is_online_get(&self.config, username)
             .await
             .map(|resp| resp.online)
     }
 
+    /// Get a list of titled-player users
     pub async fn get_titled_profiles(&self, title: api::Title) -> Result<Vec<String>> {
         pub_titled_title_get(&self.config, title).await
     }
-
+    
+    /// Get ratings, win/loss, and other stats about a player's game play, tactics, lessons and Puzzle Rush score.
     pub async fn get_user_stats(&self, username: &str) -> Result<api::PlayerStats> {
         pub_player_username_stats_get(&self.config, username).await
     }
