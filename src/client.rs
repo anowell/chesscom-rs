465ba4e5cdd3b6cc795d9150a6497ee80cd43b1e
Anthony Nowell <anowell@gmail.com>
2021-03-18 01:09:42 -0700

    Add game listing APIs

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,6 @@
 use crate::api;
 use chesscom_openapi::apis::configuration::Configuration;
-use chesscom_openapi::apis::default_api::{
-    pub_player_username_get, pub_player_username_is_online_get, pub_player_username_stats_get,
-    pub_titled_title_get,
-};
+use chesscom_openapi::apis::default_api::*;
 
 use crate::error::Result;
 
@@ -24,23 +21,39 @@ impl ChessApi {
 
     /// Get profile details about a user
     pub async fn get_profile(&self, username: &str) -> Result<api::Profile> {
-        pub_player_username_get(&self.config, username).await
+        get_player_profile(&self.config, username).await
     }
 
     /// Check if the user has been online in the last 5 minutes
-    pub async fn is_user_online(&self, username: &str) -> Result<bool> {
-        pub_player_username_is_online_get(&self.config, username)
+    pub async fn is_player_online(&self, username: &str) -> Result<bool> {
+        get_player_online_status(&self.config, username)
             .await
             .map(|resp| resp.online)
     }
 
     /// Get a list of titled-player users
-    pub async fn get_titled_profiles(&self, title: api::Title) -> Result<Vec<String>> {
-        pub_titled_title_get(&self.config, title).await
+    pub async fn get_titled_players(&self, title: api::Title) -> Result<Vec<String>> {
+        get_titled_players(&self.config, title).await
     }
     
     /// Get ratings, win/loss, and other stats about a player's game play, tactics, lessons and Puzzle Rush score.
-    pub async fn get_user_stats(&self, username: &str) -> Result<api::PlayerStats> {
-        pub_player_username_stats_get(&self.config, username).await
+    pub async fn get_player_stats(&self, username: &str) -> Result<api::PlayerStats> {
+        get_player_stats(&self.config, username).await
     }
+
+    /// Get list of a player's current daily games
+    pub async fn get_daily_chess_games(&self, username: &str) -> Result<Vec<api::DailyGame>> {
+        get_daily_chess_games(&self.config, username)
+            .await
+            .map(|resp| resp.games)
+    }
+
+    /// Get a list of games that are waiting on a specific player to move
+    pub async fn get_waiting_chess_games(&self, username: &str) -> Result<Vec<api::ToMoveGame>> {
+        get_daily_chess_games_to_move(&self.config, username)
+            .await
+            .map(|resp| resp.games)
+    }
+
+
 }
